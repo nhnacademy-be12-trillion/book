cf77b901e5c2c6f40760dadd1102af709443864e
serrot <parksh568923@gmail.com>
2025-12-12 03:23:26 +0900

    유레카 클라이언트 등록

--- src/main/java/com/nhnacademy/book/BookApplication.java
+++ src/main/java/com/nhnacademy/book/BookApplication.java
@@ -3,11 +3,13 @@ package com.nhnacademy.book;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @EnableAsync
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookApplication {
 
 	public static void main(String[] args) {
