6e4020d95e9c9726886eb09f90a2eb34e606361b
최지훈 <wlgns525359@gmail.com>
2025-12-24 21:45:14 +0900

    fix : config 수정

--- src/main/java/com/nhnacademy/book/config/AppConfig.java
+++ src/main/java/com/nhnacademy/book/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.nhnacademy.book.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,14 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        // 1. 요청 팩토리 생성
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        // 2. 타임아웃 설정 (5초) - 밀리초 단위
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+
+        // 3. 팩토리를 넣어서 RestTemplate 생성
+        return new RestTemplate(factory);
     }
 }
