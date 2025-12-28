c685b03c5bc3d4b12b01bea72a69e509c103ec4a
chosun-nhn18 <kimshh21@gmail.com>
2025-12-29 03:08:04 +0900

    fix: PointClient 오류 수정
    PutMapping -> PostMapping

--- src/main/java/com/nhnacademy/book/point/PointClient.java
+++ src/main/java/com/nhnacademy/book/point/PointClient.java
@@ -1,13 +1,14 @@
 package com.nhnacademy.book.point;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "member-service")
 public interface PointClient {
-    @PutMapping("/members/points/review")
+    @PostMapping("/members/points/review")
     void awardReviewPoints(@RequestHeader("X-Member-Id") Long memberId,
                            @RequestBody ReviewPointRequest request);
 }
